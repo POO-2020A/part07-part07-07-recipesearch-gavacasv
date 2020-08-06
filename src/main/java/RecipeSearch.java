f80904734f4cd765f4897c7f3dab4551955216ce
gavacasv <58041959+gavacasv@users.noreply.github.com>
2020-08-05 21:20:22 -0500

    Add files via upload

--- src/main/java/RecipeSearch.java
+++ src/main/java/RecipeSearch.java
@@ -8,23 +8,17 @@ public class RecipeSearch {
 
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Recetario> recetario = new ArrayList<>();
+        ArrayList<String> recipes = new ArrayList<>();
+        recipes.add("");
         System.out.print("File to read: ");
         String file= scanner.nextLine();
+        String line= "";
         try (Scanner filer = new Scanner(Paths.get(file))) {
-           Recetario receta=new Recetario();
- 
+           
             while (filer.hasNextLine()) {
-                String linea=filer.nextLine();
-                if(!linea.isEmpty()){
-                    receta.add(linea);
-                }
-                else{
-                    recetario.add(receta);
-                    receta=new Recetario();
-                }
+                recipes.add(filer.nextLine());
             }
-            recetario.add(receta);
+            
             String command = "";
             System.out.print("\nCommands: "
                         + "\nlist - list the recipes"
@@ -37,44 +31,96 @@ public class RecipeSearch {
 
                 System.out.print("\nEnter command: ");
                 command = scanner.nextLine();
-                String found = "\n", find;
+                String found , find;
                 switch (command) {
                     case "list":
-                        for (Recetario recipe : recetario) {
-                            System.out.println(recipe.toString());
-                        }
+                        int i=0;
+                        do{
+                            if(!recipes.get(i).isEmpty()){
+                                i++;
+                            }
+                            else{
+                                System.out.println(recipes.get(++i)+", cooking time: "+recipes.get(++i));
+                            }
+                        }while(i<recipes.size());
                         break;
                     case "find name":
                         System.out.print("Searched word: ");
                         find = scanner.nextLine();
+                        found="\n";
+                        String auxiliar="";
+                        i=0;
+                        String[] words;
 
-                        for (Recetario recipe : recetario) {
-                            if (recipe.getName().contains(find)) {
-                                found += recipe.toString() + "\n";
+                        do{
+                            if (recipes.get(i).isEmpty()) {
+                                int j=i+1;
+                                auxiliar = recipes.get(++i) + ", cooking time: " + recipes.get(++i);
+                                words = recipes.get(j).split(" "); 
+                                for(String word:words){
+                                    if (word.contains(find)) {
+                                        
+                                       found +=  auxiliar+"\n";
+                                    }
+                                }
                             }
-                        }
+
+                            else{
+                                i++;
+                                
+                            }
+                        }while(i<recipes.size() );
                         System.out.println("Recipes: " + found);
+       
                         break;
                     case "find cooking time":
+                        found="\n";
                         System.out.print("Max cooking time: ");
-                        int time = Integer.valueOf(scanner.nextLine());
-
-                        for (Recetario recipe : recetario) {
-                            if (recipe.getTime() <= time) {
-                                found +="\n"+ recipe.toString() ;
+                        
+                        i=0;
+                        find = scanner.nextLine();
+                        int time=Integer.valueOf(find);
+                        int timer;
+                    
+                        do{
+                            if (!recipes.get(i).isEmpty()) {
+                                i++;
+                            } else {
+                                auxiliar = recipes.get(++i)+", cooking time: "+recipes.get(++i);
+                                timer=Integer.valueOf(recipes.get(i));
+                                if (timer <= time) {
+                                    found += "\n" + auxiliar;
+                                }
                             }
-                        }
+                        }while (i < recipes.size() );
+                                          
                         System.out.println("Recipes: " + found);
                         break;
                     case "find ingredient":
+                        found="";
                         System.out.print("Ingredient:");
                         find = scanner.nextLine();
+                        auxiliar = "";
+                        i = 0;
+                        
 
-                        for (Recetario recipe : recetario) {
-                            if (recipe.ingredients(find)) {
-                                found += recipe.toString() + "\n";
+                        do {
+                            if (recipes.get(i).isEmpty()) {
+                                
+                                auxiliar = recipes.get(++i)+", cooking time: "+recipes.get(++i);
+                            } else {
+                                
+                                words = recipes.get(i).split(" ");
+                                for (String word : words) {
+                                    if (word.equals(find)) {
+                                       
+                                        found += "\n" + auxiliar ;
+                                    }
+                                }
+                                i++;
                             }
-                        }
+                        }while (i < recipes.size() );
+
                         System.out.println("Recipes: " + found);
                         break;
                 }
@@ -83,9 +129,8 @@ public class RecipeSearch {
         }catch (Exception e) {
             
         }
-        
-        
-
     }
+    
+    
 
 }
